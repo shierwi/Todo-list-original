f0d3a8753fd719abc595e14f0838388df38e42d0
Tecnologías de la Información <tic.calama@inacap.cl>
2025-07-17 19:40:34 -0400

    Funcionalidad update

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,16 +1,46 @@
 import React, { Fragment } from "react";
-export function TodoItem({todo, cambiarEstado}) {
-    const {id, task, completed} = todo;
 
-    const fnCambiarEstado = () => {
-        cambiarEstado(id)
-    }
+export function TodoItem({
+  todo,
+  cambiarEstado,
+  editarTarea,
+  editTaskId,
+  newTask,
+  setNewTask,
+}) {
+  const { id, task, completed } = todo;
 
-    return (
-        <Fragment>
-            <li className="list-group-item">
-                <input onChange={fnCambiarEstado} type="checkbox" checked={completed} className="form-check-input me-2"/>
-                {task}</li>
-        </Fragment>
-    )
+  const fnCambiarEstado = () => {
+    cambiarEstado(id);
+  };
+
+  const handleEditar = () => {
+    editarTarea(id, task);
+  };
+
+  return (
+    <Fragment>
+      <li className="list-group-item">
+        <input
+          onChange={fnCambiarEstado}
+          type="checkbox"
+          checked={completed}
+          className="me-2"
+        />
+        {editTaskId === id ? (
+          <input
+            type="text"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            className="form-control d-inline-block w-auto"
+          />
+        ) : (
+          task
+        )}
+        <button onClick={handleEditar} className="btn btn-warning ms-2">
+          <i className="bi bi-pencil"></i>
+        </button>
+      </li>
+    </Fragment>
+  );
 }
